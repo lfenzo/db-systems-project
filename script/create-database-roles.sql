257b784862fabc2597bd59c525c8d345f18648f4
lfenzo <lfenzo@protonmail.com>
2022-09-25 14:37:45 -0300

    Updated create-database-roles script

--- script/create-database-roles.sql
+++ script/create-database-roles.sql
@@ -1,22 +1,53 @@
+--
 -- admin user
-CREATE ROLE admin
-WITH SUPERUSER;
+--
+CREATE ROLE administrador
+WITH LOGIN NOINHERIT CREATEDB CREATEROLE;
+
+REVOKE EXECUTE ON FUNCTION public.salary_range_mix_adherence FROM PUBLIC;
+REVOKE EXECUTE ON FUNCTION public.product_category_volume_adherence FROM PUBLIC;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO administrador WITH GRANT OPTION;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO administrador WITH GRANT OPTION;
 
 
+--
 -- seller role
+--
 CREATE ROLE seller
-WITH NOINHEIRIT;
+WITH NOINHERIT;
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO seller;
-GRANT INSERT ON purchase TO seller;
+GRANT INSERT, UPDATE, DELETE ON purchase TO seller;
 
 
-
+--
 -- analyst role
+--
 CREATE ROLE analyst
-WITH NOINHEIRIT;
+WITH NOINHERIT;
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO analyst;
-GRANT INSERT ON recommendation TO analyst;
+GRANT INSERT, UPDATE, DELETE ON recommendation TO analyst;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO analyst;
+
+
+--
+-- customer
+--
+CREATE ROLE customer
+WITH NOINHERIT;
+
+GRANT SELECT ON recommendation_view, purchase_view TO customer;
+GRANT SELECT ON product, category TO customer;
+
+
+--
+-- guest role
+--
+CREATE role guest
+with noinherit;
+
+GRANT SELECT ON product, category TO guest;
+
 
